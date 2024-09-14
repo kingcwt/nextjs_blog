803d8fe889982c83182f0dfa32583b07bec04798
kingcwt <kingcwt@qq.com>
2024-09-14 17:32:00 +0800

    素数

--- test/algorithm.js
+++ test/algorithm.js
@@ -1,20 +1,33 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// const isprimeFunc = (item) => {
+//   if (item <= 1) {
+//     return false
+//   }
+
+//   for (let i = 2; i <= Math.sqrt(item); i++) {
+//     if (item % i === 0) {
+//       return false
+//     }
+//   }
+
+//   return true
+// }
+// const main = (arr) => {
+//   return arr.filter(isprimeFunc)
+// }
+
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-const isprimeFunc = (item) => {
-  if (item <= 1) {
-    return false
-  }
-
-  for (let i = 2; i <= Math.sqrt(item); i++) {
-    if (item % i === 0) {
-      return false
-    }
-  }
-
-  return true
-}
 const main = (arr) => {
-  return arr.filter(isprimeFunc)
+  return arr.filter((item) => {
+    if (item <= 1) return false
+    for (let i = 2; i < item; i++) {
+      if (item % i === 0) {
+        return false
+      }
+    }
+    return true
+  })
 }
 
 console.log(main(numbers))
+
