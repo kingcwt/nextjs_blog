642b14ee903d1ad0fd2263a47ca96401fb9ea013
kingcwt <kingcwt@qq.com>
2024-12-24 16:47:59 +0800

    1 找单独的数

--- test/algorithm.js
+++ test/algorithm.js
@@ -1,44 +1,39 @@
-// // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-// // const isprimeFunc = (item) => {
-// //   if (item <= 1) {
-// //     return false
-// //   }
-//
-// //   for (let i = 2; i <= Math.sqrt(item); i++) {
-// //     if (item % i === 0) {
-// //       return false
-// //     }
-// //   }
-//
-// //   return true
-// // }
-// // const main = (arr) => {
-// //   return arr.filter(isprimeFunc)
-// // }
-//
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-// const PrimeCheck = (n) => {
-//   // input: n: int
-//   // output: boolean
-//   if (n === 1) return false
-//   if (n === 0) return false
-//   if (n === 2) return true
-//   if (n % 2 === 0) return false
-//
-//   for (let i = 3; i * i <= n; i += 2) {
-//     if (n % i === 0) {
-//       return false
-//     }
-//   }
-//   return true
-// }
-//
-//
-// console.log(PrimeCheck(13))
-//
 
-for (var i = 0; i < 3; i++) {
-  setTimeout(function log() {
-    console.log(i)
-  }, 1000)
+
+
+
+
+
+
+
+
+
+/**
+ * @description 判断一个数组中的素数
+ * @param {*} numbers
+ */
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+const isPrime = (numbers) =>{
+   
+    const result = []
+    for(let i =2;i<numbers.length;i++){
+      let item = numbers[i]
+      
+      let cur = []
+      for(let j = 2;j<item;j++){
+        if(item%j===0){
+           cur.push(j)
+        }
+      }
+
+      if(cur.length===0){
+        result.push(item)
+      }
+       
+    }
+
+    return result
 }
+
+console.log(isPrime(numbers))
